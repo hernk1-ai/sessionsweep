320eaa75747d80c8fbcf54523527e7c9149ad7ab
hernk1-ai <hernk1@gmail.com>
2026-06-10 01:10:33 -0700

    Initial Commit

---
+++ SessionSweep/SessionSweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionSweepApp.swift
+//  SessionSweep
+//
+//  Created by Kris Hernholm on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SessionSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
